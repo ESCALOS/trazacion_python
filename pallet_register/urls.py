f16675d7a0970e41ea1f5cd00ef86b6a388dc377
sergio-dev96 <sergiobbr1996@gmail.com>
2022-11-10 13:56:20 -0500

    Testeando instascan js

--- pallet_register/urls.py
+++ pallet_register/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('signup/', views.ingreso, name='signup')
-    
+    path('login/', views.autenticacion, name='login')
 ]
--- pallet_register/views.py
+++ pallet_register/views.py
@@ -1,31 +1,31 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login,authenticate
 from django.db import IntegrityError
 
-def ingreso(request):
+def autenticacion(request):
     
     if request.method == "GET":
-        return render(request, 'signup.html',{
+        return render(request, 'login.html',{
             'form' : UserCreationForm
         })
     else:
-        if request.POST['password1'] == request.POST['password2']:
-            try:
-                user = User.objects.create_user(username=request.POST['username'],password=request.POST['password1'])
-                user.save()
+        try:
+            user = authenticate(username=request.POST['user'], password=request.POST['password'])
+            if user is not None:    
                 login(request,user)
                 return redirect('index')
-            except IntegrityError:
-                return render(request, 'signup.html',{
-                    'form' : UserCreationForm,
-                    'error' : 'El usuario ya existe'
-                })
-        return render(request, 'signup.html',{
-            'form' : UserCreationForm,
-            'error' : 'Las contraseñas no coinciden'
-        })
+            else:
+                return render(request, 'login.html',{
+                'form' : UserCreationForm,
+                'error' : 'Contraseña incorrecta'
+            })
+        except IntegrityError:
+            return render(request, 'login.html',{
+                'form' : UserCreationForm,
+                'error' : 'El usuario ya existe'
+            })
     
 def index(request):
     return render(request, 'index.html')
